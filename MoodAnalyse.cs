fc05ca0c21e99104aea3f357510629313fac2582
kamalakars04 <kamalakars04@gmail.com>
2020-10-10 11:19:35 +0530

    Add project files.

---
+++ MoodAnalyse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    class MoodAnalyse
+    {
+        //variable
+        private string message;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message.ToLower();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to Mood Analyser");
+            Console.WriteLine("Enter the state of mood");
+            string message = Console.ReadLine();
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+        }
+    }
+}
