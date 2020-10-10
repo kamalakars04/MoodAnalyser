242484ffb99276939855ea3c59c37bf394f8013f
kamalakars04 <kamalakars04@gmail.com>
2020-10-10 12:40:34 +0530

    [Kamalakar].[Refactor].Added ParameterizedConstuctor to UC1_AnalyseMood

--- MoodAnalyse.cs
+++ MoodAnalyse.cs
@@ -8,12 +8,18 @@ namespace MoodAnalyser
     public class MoodAnalyse
     {
         private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private string message;
+        public MoodAnalyse(string message)
+        {
+            logger.Info("Initiated message through a contructor");
+            this.message = message.ToLower();
+        }
         /// <summary>
         /// Analyses the mood.
         /// </summary>
         /// <param name="message">The message.</param>
         /// <returns></returns>
-        public string AnalyseMood(string message)
+        public string AnalyseMood()
         {
             logger.Debug("User entered the analyse mood method");
             if (message.ToLower().Contains("sad"))
